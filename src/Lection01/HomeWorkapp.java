d2628c07cfdd7a9ba1332cc09c87436093f59a88
Tanya <Tanya@localhost.localdomain>
2021-05-03 12:49:57 +0300

    home1

---
+++ src/Lection01/HomeWorkapp.java
@@ -0,0 +1,35 @@
+package Lection01;
+
+public class HomeWorkapp {
+    public static void main(String[] args) {
+            System.out.println("Orange");
+            System.out.println("Banana");
+            System.out.println("Apple");
+            int a = 15;
+            int b = 83;
+            int c = a+b;
+            if (c >= 0)
+                System.out.println("Сумма положительная");
+
+            if (c <= 0)
+            System.out.println("Сумма отрицательная");
+            int value = 101;
+             System.out.println("value");
+             if (value <= 0)
+                 System.out.println("Красный");
+             if (value <= 100)
+                 System.out.println("Желтый");
+             if (value > 100)
+                 System.out.println("Зеленый");
+
+             int t = 185;
+             int k = 210;
+             if (t >= k)
+                 System.out.println("t >= k");
+             if (t < k)
+                 System.out.println("t < k");
+    }
+}
+
+
+
